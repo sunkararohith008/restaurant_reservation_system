96f2ef8cc94a1b72ef1d2a36dfb2e69903dd93cd
Arshveer0708 <64243859+Arshveer0708@users.noreply.github.com>
2021-08-15 18:58:13 -0400

    Added reservation componrnts

--- frontend-material/src/app/app.module.ts
+++ frontend-material/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { AddCustomerComponent } from './components/add-customer/add-customer.component';
 import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
 import { CustomersListComponent } from './components/customers-list/customers-list.component';
+import { AddReservationComponent } from './components/add-reservation/add-reservation.component';
+import { EditReservationComponent } from './components/edit-reservation/edit-reservation.component';
+import { ReservationsListComponent } from './components/reservations-list/reservations-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddCustomerComponent,
     EditCustomerComponent,
-    CustomersListComponent
+    CustomersListComponent,
+    AddReservationComponent,
+    EditReservationComponent,
+    ReservationsListComponent
   ],
   imports: [
     BrowserModule,
