e902b4821eb9c442eba87b9eeb0b5fe9b4377355
Rohith Preetham Sunkara <sunkararohith008@gmail.com>
2021-08-15 19:36:15 -0400

    Added customer.ts

---
+++ frontend-material/src/app/shared/customer.ts
@@ -0,0 +1,8 @@
+export interface Customer {
+    _id: String;
+    customer_fname: String;
+    customer_lname: String;
+    customer_gender: String;
+    customer_address: String;
+    customer_phone: String;
+ }
